c75a4bce8ee1452eaee7edb2b8724a6d533df2cb
Anil Nanda <anilthekkoote@gmail.com>
2022-04-22 11:38:23 +0400

    Updating test cases to handle multiple cases

--- app/main.py
+++ app/main.py
@@ -6,3 +6,6 @@ app = FastAPI()
 @app.get("/")
 def getPost():
     return "Hello world!"
+
+def add(a, b):
+    return a+b
--- unit_tests/api_test.py
+++ unit_tests/api_test.py
@@ -1,4 +1,17 @@
-from app.main import getPost
+import pytest
+from app.main import getPost, add
+import pytest
+
+
+@pytest.mark.parametrize("num1, num2, expectedResult",[
+  (1,2,3),
+  (12,3,15),
+  (34,12,46)
+])
+def test_add(num1, num2, expectedResult):
+  print("testing add function")
+  assert add(num1, num2) == expectedResult
+
 
 
 def test_getPost():
